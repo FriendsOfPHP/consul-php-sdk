1005d5f7d16110d5fac418705ee9bc17781fcc8e
Grégoire Pineau <lyrixx@lyrixx.info>
2022-05-03 20:21:24 +0200

    Add GitHub Action

--- Consul/Tests/Services/KVTest.php
+++ Consul/Tests/Services/KVTest.php
@@ -73,7 +73,9 @@ class KVTest extends TestCase
         $this->kv->delete('test/my/key');
 
         $this->expectException(ClientException::class);
-        $this->expectExceptionMessageMatches('/404/');
+        if (method_exists($this, 'expectExceptionMessageMatches')) {
+            $this->expectExceptionMessageMatches('/404/');
+        }
 
         $this->kv->get('test/my/key');
     }
